ac86e2900f1eba0c97c812aa10dd2319353f2108
HuangRuiLin <wong_sl@163.com>
2019-12-03 16:00:48 +0800

    alter picturegallery, add upload, add params {module, multiple}

--- src/components/PictureGallery/README.md
+++ src/components/PictureGallery/README.md
@@ -10,12 +10,9 @@
 ```
 <template>
     <picture-gallery
-        title="我的图库"
-        :width="900"
         :visible="isShow"
-        @update:visible="isShow = $event"
-        module="all"
         @ok="handleSelected"
+        @cancel="isShow = false"
     >
     </picture-gallery>
 </template>
@@ -35,15 +32,16 @@ export default {
 ```
 
 ####参数
-|  名      | 类型     | 描述        | 默认值 |
-|  ----    | ----    | ---         | ---   |
-| title    | String  |  模态框标题  |       |
-| width    | Number  |  模态框宽度  | 900   |
-| visible  | Boolean | 是否显示 | false |
-| module   | String  | 指定图片模块( 目前可选 ["all", "employee", "feed", "lottery", "goods"] ) | all |
+|  名      | 类型     | 描述        | 默认值   |
+|  ----    | ----    | ---         | ---     |
+| title    | String  |  模态框标题  | 图库     |
+| width    | Number  |  模态框宽度  | 900     |
+| visible  | Boolean | 是否显示     | false   |
+| multiple | Boolean | 是否允许多选     | true    |
+| module   | String  | 指定默认模块( 目前可选 ["all", "employee", "feed", "lottery", "goods"] ) | all |
 
 ####事件
-|  名             | 回调参数               | 描述              |
-|  ----           | ----                  | ----             |
-| update:visible  | function(visible)     | 显示或隐藏后的回调 |
-| ok              | function(pictureList) | 确定选择之后的回调 |
+|  名      | 回调参数               | 描述              |
+|  ----    | ----                  | ----             |
+| cancel   | function()            | 显示或隐藏后的回调 |
+| ok       | function(pictureList) | 确定选择之后的回调 |
---
+++ src/components/PictureGallery/components/PageUpload.vue
@@ -0,0 +1,153 @@
+<template>
+    <div class="page-upload">
+        <a-spin :spinning="loading">
+            <div class="page-main">
+                <a-form :form="form">
+                    <a-form-item label="选择模块" v-bind="formItemLayout">
+                        <a-select
+                                v-decorator="['module', optionConfig.module]"
+                                placeholder="选择模块"
+                                @change="handleSelectChange"
+                        >
+                            <a-select-option
+                                    :value="item.attach_module"
+                                    v-for="(item, idx) in moduleList"
+                                    :key="idx"
+                            >
+                                {{item.module_name}}
+                            </a-select-option>
+                        </a-select>
+                    </a-form-item>
+                    <a-form-item label="上传图片" v-bind="formItemLayout">
+                        <a-upload
+                                :action="uploadUrl"
+                                :data="uploadAddData"
+                                :headers="uploadHeader"
+                                listType="picture-card"
+                                :fileList="fileList"
+                                @preview="handlePreview"
+                                @change="handleChange"
+                                :remove="handleRemove"
+                        >
+                            <a-icon type="plus" />
+                            <div class="ant-upload-text">直接上传</div>
+                        </a-upload>
+                    </a-form-item>
+                </a-form>
+            </div>
+            <div class="page-footer">
+                <a-button @click="handleCancel">返回</a-button>
+<!--                <a-button type="primary" @click="handleConfirm">确定</a-button>-->
+            </div>
+        </a-spin>
+        <a-modal :visible="previewVisible" :footer="null" @cancel="previewVisible = false">
+            <img style="width: 100%" :src="previewImage" />
+        </a-modal>
+    </div>
+</template>
+
+<script>
+    import {deleteAttach} from "@/api/attach"
+    export default {
+        name: "PageUpload",
+        props: {
+            moduleList: {
+                type: Array,
+                default: () => []
+            },
+            activeModule: {
+                type: String,
+                default: ''
+            }
+        },
+        data() {
+            return {
+                loading: false,
+                form: this.$form.createForm(this, { name: 'pageUpload' }),
+                formItemLayout: {
+                    labelCol: {
+                        sm: { span: 3 },
+                    },
+                    wrapperCol: {
+                        sm: { span: 12 },
+                    },
+                },
+                optionConfig: {
+                    module: { rules: [{ required: true, message: '请选择模块！' }], initialValue: this.activeModule }
+                },
+                fileList: [],
+                previewVisible: false,
+                previewImage: '',
+                selectedModule: this.activeModule,
+                uploadUrl: "http://192.168.101.115:8089/api/attach",
+                uploadHeader: {
+                    company: this.$ls.get("company").id
+                }
+            }
+        },
+        methods: {
+            uploadAddData() {
+                return {
+                    module: this.selectedModule
+                }
+            },
+            handleSelectChange(value) {
+                this.selectedModule = value
+            },
+            handlePreview(file) {
+                this.previewImage = file.url || file.thumbUrl;
+                this.previewVisible = true;
+            },
+            handleChange({ fileList }) {
+                this.fileList = fileList;
+            },
+            async handleRemove(file) {
+                let result = false
+                let attach_id = file.response.data.attach_id
+                await deleteAttach(attach_id).then(res => {
+                    if(res.data) {
+                        if(res.data.status == 'true') {
+                            this.$message.success(res.data.message)
+                            result = true
+                        } else {
+                            this.$message.error(res.data.message)
+                            result = false
+                        }
+                    } else {
+                        this.$message.error("删除失败")
+                        result = false
+                    }
+                }).catch(() => {
+                    this.$message.error("删除失败")
+                    result = false
+                })
+                return result
+            },
+            handleConfirm() {
+                this.$emit("ok")
+            },
+            handleCancel() {
+                this.$emit("cancel")
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    @import "~@/styles/global.less";
+    .page-main {
+        height: 500px;
+    }
+    .page-footer {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        margin-top: 20px;
+
+        .ant-btn {
+            & + .ant-btn {
+                margin-left: 25px;
+            }
+        }
+    }
+</style>
